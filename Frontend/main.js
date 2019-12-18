0a878d2847b99ade04abf14aeae64f2452f59119
phenix1229 <paul.garay@codeimmersives.com>
2019-12-17 20:34:09 -0500

    got postUser function to console.log list of users with new user added

--- Frontend/main.js
+++ Frontend/main.js
@@ -4,10 +4,10 @@ xhr.addEventListener('loadend', (event) => {
   console.log(event.target.response);
 })
 
-// const getUser = ()=>{
-//     xhr.open('GET','http://localhost:3000/data')
-//     xhr.send()
-// }
+const getUser = () => {
+    xhr.open('GET','http://localhost:3000/data')
+    xhr.send()
+}
 
 // const getFriend = ()=>{
     
@@ -16,7 +16,29 @@ xhr.addEventListener('loadend', (event) => {
 //     xhr.send()
 // }
 
-const getFriend = (i)=>{
-    xhr.open('GET',`'http://localhost:3000/data/${i}'`)
-    xhr.send()
+const getFriend = (i) => {
+    xhr.open('GET',`http://localhost:3000/data/${i}`);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      let data = xhr.response;
+      console.log(data.friends);
+    };
+    xhr.send();
+}
+
+const postUser = (age, eyeColor, name) => {
+  xhr.open('GET',`http://localhost:3000/data`);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    let users = [...xhr.response];
+    user = {
+      "id": users.length,
+      "age": age,
+      "eyeColor": eyeColor,
+      "name": name
+    };
+    users.push(user);
+    console.log(users);
+  };
+  xhr.send();
 }
